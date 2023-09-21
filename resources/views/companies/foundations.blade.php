e2a5ac8f26b09dc66f3d866cd34085781174a8ef
Lengyel Bálint <blengyel89@gmail.com>
2023-09-22 00:55:31 +0200

    companyFoundationsByDate

--- resources/views/companies/foundations.blade.php
+++ resources/views/companies/foundations.blade.php
@@ -1,8 +1,3 @@
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" />
-<script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 <table>
     <thead>
         <tr>
